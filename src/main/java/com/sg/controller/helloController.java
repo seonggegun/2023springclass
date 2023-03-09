c6708ca1b1a92e5a7af5ad1623525eae7025c2db
seonggegun <seonggegun@naver.com>
2023-03-09 14:54:34 +0900

    정적콘텐츠관리

---
+++ src/main/java/com/sg/controller/helloController.java
@@ -0,0 +1,19 @@
+package com.sg.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class helloController {
+	@GetMapping("/html")
+	public String html() {
+		System.out.println("HTML 파일이 요청됨");
+		return "redirect:starthere.html";
+	}
+	
+	@GetMapping("/img")
+	public String img() {
+		System.out.println("이미지파일이 요청됨");
+		return "redirect:img/A.png";
+	}
+}
